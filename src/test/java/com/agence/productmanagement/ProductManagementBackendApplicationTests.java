6741361147e122ba8a5ed68030dc48115ab40325
Gleibson Oliveira <gleibson.rodrigo>
2025-05-04 23:45:32 -0300

    Initial commit

---
+++ src/test/java/com/agence/productmanagement/ProductManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agence.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
